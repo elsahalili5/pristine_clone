7459361d01c6e98b4dffb78f9d0d5deff08d73cc
elsahalili5 <elsahalilii5@gmail.com>
2023-08-09 16:15:10 +0200

    ⚡️Implemented error message for non-logged-in order attempts

--- js/checkout.js
+++ js/checkout.js
@@ -45,62 +45,77 @@ document.addEventListener("DOMContentLoaded", function () {
   orderBtn.addEventListener("click", function () {
     let billingDetails = {};
     let hasError = false;
-    errorsElement.innerHTML = "";
 
-    checkoutInputs.forEach((input) => {
-      billingDetails[input.name.replace("billing-", "")] = input.value;
+    const placeOrderButton = document.getElementById("order-btn");
 
-      if (input.value === "") {
+    placeOrderButton.addEventListener("click", () => {
+      if (loggedInUserData === null || loggedInUserData.length === 0) {
         hasError = true;
-        input.classList.add("input-error");
-
-        if (input.id === "billing-country") {
-          errorsElement.innerHTML += `<li >Country is a required field!</li>`;
-        } else {
-          errorsElement.innerHTML += `<li >${input.placeholder} is a required field!</li>`;
-        }
+        errorsElement.innerHTML = "<li>Please log in to place an order.</li>";
+        return;
       } else {
-        input.classList.remove("input-error");
+        errorsElement.innerHTML = "";
+        checkoutInputs.forEach((input) => {
+          billingDetails[input.name.replace("billing-", "")] = input.value;
+
+          if (input.value === "") {
+            hasError = true;
+            input.classList.add("input-error");
+
+            if (input.id === "billing-country") {
+              errorsElement.innerHTML += `<li >Country is a required field!</li>`;
+            } else {
+              errorsElement.innerHTML += `<li >${input.placeholder} is a required field!</li>`;
+            }
+          } else {
+            input.classList.remove("input-error");
+          }
+        });
       }
+
+      if (!hasError) {
+        const orderedItems = JSON.parse(localStorage.getItem("orderedItems")) || [];
+        const creditCardCheckbox = document.getElementById("credit-card-checkbox");
+        const paypalCheckbox = document.getElementById("paypal-checkbox");
+        let selectedPaymentMethod = "";
+        let calculatedTotalPrice = 0;
+        cartItems.forEach((cartItem) => {
+          calculatedTotalPrice += cartItem.price * cartItem.quantity;
+        });
+        if (creditCardCheckbox.checked) {
+          selectedPaymentMethod = "Credit Card";
+        } else if (paypalCheckbox.checked) {
+          selectedPaymentMethod = "PayPal";
+        }
+
+        creditCardCheckbox.addEventListener("change", function () {
+          paypalCheckbox.checked = false;
+        });
+
+        paypalCheckbox.addEventListener("change", function () {
+          creditCardCheckbox.checked = false;
+        });
+
+        const currentDate = new Date();
+        const formattedDate = `${currentDate.getFullYear()}-${
+          currentDate.getMonth() + 1
+        }-${currentDate.getDate()} ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
+        const newOrder = {
+          user: loggedInUserData,
+          billingDetails: billingDetails,
+          orderItems: cartItems,
+          paymentMethod: selectedPaymentMethod,
+          total: calculatedTotalPrice,
+          date: formattedDate,
+        };
+        orderedItems.push(newOrder);
+        localStorage.setItem("orderedItems", JSON.stringify(orderedItems));
+      }
+      placeOrderButton.addEventListener("click", () => {
+        localStorage.removeItem("cartItems");
+        orderedItemsContainer.innerHTML = "";
+        subtotalSpan.innerHTML = "0.00";
+      });
     });
-
-    if (!hasError) {
-      const orderedItems = JSON.parse(localStorage.getItem("orderedItems")) || [];
-      const creditCardCheckbox = document.getElementById("credit-card-checkbox");
-      const paypalCheckbox = document.getElementById("paypal-checkbox");
-      let selectedPaymentMethod = "";
-      let calculatedTotalPrice = 0;
-      cartItems.forEach((cartItem) => {
-        calculatedTotalPrice += cartItem.price * cartItem.quantity;
-      });
-      if (creditCardCheckbox.checked) {
-        selectedPaymentMethod = "Credit Card";
-      } else if (paypalCheckbox.checked) {
-        selectedPaymentMethod = "PayPal";
-      }
-
-      creditCardCheckbox.addEventListener("change", function () {
-        paypalCheckbox.checked = false;
-      });
-
-      paypalCheckbox.addEventListener("change", function () {
-        creditCardCheckbox.checked = false;
-      });
-
-      const currentDate = new Date();
-      const formattedDate = `${currentDate.getFullYear()}-${
-        currentDate.getMonth() + 1
-      }-${currentDate.getDate()} ${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`;
-      const newOrder = {
-        user: loggedInUserData,
-        billingDetails: billingDetails,
-        orderItems: cartItems,
-        paymentMethod: selectedPaymentMethod,
-        total: calculatedTotalPrice,
-        date: formattedDate,
-      };
-      orderedItems.push(newOrder);
-      localStorage.setItem("orderedItems", JSON.stringify(orderedItems));
-    }
   });
 });
